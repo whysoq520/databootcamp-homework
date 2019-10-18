e9b5f42173172ed4fd95636cf3de994b806b19dc
whysoq520 <53708390+whysoq520@users.noreply.github.com>
2019-10-17 23:56:57 -0400

    Update main.py

--- python-challenge/PyPoll/main.py
+++ python-challenge/PyPoll/main.py
@@ -1,7 +1,7 @@
 # Import required packages
 import csv
 import os
-import statistics
+
 
 #paths for read and write file
 csvpath= os.path.join("Resources", "election_data.csv")
@@ -11,83 +11,86 @@ csvpath= os.path.join("Resources", "election_data.csv")
 voter_id_list = []
 County_list= []
 candidate_list = []
+total_vote_number=0
 
-def unique(list):
+def unique_data(list_1):
     unique_list = []     
-    for x in list: 
+    for x in list_1: 
         # check if exists in unique_list or not 
         if x not in unique_list: 
             unique_list.append(x) 
-        print(unique_list) 
+    return (unique_list) 
 
 
 #read file for data
 with open(csvpath, newline='') as election:
-  reader = csv.reader(election, delimiter=',')
+    reader = csv.reader(election, delimiter=',')
 
-  #there is header, read the head row first
-  header = next(election)
-  print(f"Header:{header}") 
+    #there is header, read the head row first
+    header = next(election)
+    print(f"Header:{header}") 
 
-  for row in reader:
-    # A complete list of candidates who received votes
-    voter_id_list = voter_id_list + [row[0]]
-    #County_list.append(row[1])
-    candidate_list.append(row[2])
-    unique(voter_id_list)
-    #unique(County_list)
-    unique(candidate_list)
-   #['Khan', 'Correy', 'Li', "O'Tooley"]
+    for row in reader:
+        #total vote count
+        total_vote_number=total_vote_number+1
+
+        #append to candidate_list
+        candidate_list.append(row[2])
+
+unique_cand=unique_data(candidate_list)
+
+d={}
+
+for cand in unique_cand:
+    d["{0}".format(cand)]=[candidate_list.count(cand), candidate_list.count(cand)/total_vote_number*100]
+Khan_count = d['Khan'][0]
+Correy_count = d['Correy'][0]
+Li_count = d['Li'][0]
+O_Tooley_count = d["O'Tooley"][0]
+
+max_vote = max(Khan_count, Correy_count, Li_count, O_Tooley_count)
+
+Election_Results = os.path.join("Election Results.txt")
+with open(Election_Results, "w") as txt_file:
+    txt_file.write("Election Results\n")
+    txt_file.write("------------------------------------\n")
+    txt_file.write("Total Votes: " + str(total_vote_number))
+    print("Election Results\n")
+    print("------------------------------------\n")
+    print("Total Votes: " + str(total_vote_number))
+    for k,v in d.items():
+        txt_file.write
+        print((k + ": " + str(v[1]) + "% (" + str(v[0]) + ")\n"))
+        if d[k][0]==max_vote:
+            winner = k
+    txt_file.write("Winner: " + winner)
+    print("Winner: " + winner)
+    print("------------------------------------\n")
+    txt_file.write("------------------------------------\n")
+    # print (k, v)
+
+# print(d)
 
 
+# output = (
+# f"Election Results\n"
+# f"-----------------\n"
+# f"Total Votes:: {total_vote_number}\n"
+# f"Khan: {d['Khan'][1]} ({d['Khan'][0]})\n"
+# f"Correy: {d['Correy'][1]} ({d['Correy'][0]})\n"
+# f"Li: {d['Li'][1]} ({d['Li'][0]})\n"
+# #"{0}: {1} ({2})". format( "O'Tooley", d["O'Tooley"][1], d["O'Tooley"][0] )
+# f"OTooley: {d["OTooley"][1]} ({d["OTooley"][0]})\n"
+# f"-----------------\n"
+# f"Winner:{winner}\n"
+# f"-----------------\n"
+# )
 
-    
- 
-    # The total number of votes cast      
-    total_vote_number = len(voter_id_list)
-    #print(voter_id_list)
-    print(total_vote_number) #571867
-    
+# # #output path 
+# Election_Results = os.path.join("Election Results.txt")
+# # # # Print all of the results (to terminal)
+# # print(output)
 
-
-  # The total number of votes each candidate won
-  #if row[2] not in candidate_list:
-      #candidate_list.append(row[2]) 
-      #print(candidate_list)
-  #actual_canditate_list = ['Khan', 'Correy', 'Li', "O'Tooley"]
-  Khan_count = candidate_list.count("Khan")
-  Correy_count = candidate_list.count("Correy")
-  Li_count = candidate_list.count(actual_candidate_list[2])
-  O_Tooley_count = candidate_list.count("O'Tooley")
-
-
-  
-
- # The percentage of votes each candidate won
-  Khan_percentage = Khan_count/total_vote_number
-  Correy_percentage = Correy_count/total_vote_number
-  Li_percentage = Li_count / total_vote_number
-  O_Tooley_percentage = O_Tooley_count / total_vote_number
-
-# The winner of the election based on popular vote.
-winner = max [Khan_count,Li_count,O_Tooley_count,Correy_count]
-# Generate Election Results
-#output = (
-    #f"Election Results\n"
-    #f"-----------------\n"
-    #f"Total Votes:: {total_vote_number}\n"
-    #f"Khan: {Khan_percentage ({Khan_count})
-     # Correy: 20.000% (704200)
-      #Li: 14.000% (492940)
-      #O'Tooley: 3.000% (105630): \n"
-    #f"-----------------\n"
-    #f"Winner:{winner}: {avg_letter_count}\n"
-    #f"-----------------\n"
-#output path 
-#Election_Results = os.path.join("Election Results.txt")
-# Print all of the results (to terminal)
-#print(output)
-
-# Save the results to analysis text file
-#with open(Election_Results, "a") as txt_file:
-    #txt_file.write(output)
+# # # # Save the results to analysis text file
+# with open(Election_Results, "w") as txt_file:
+#     txt_file.write(output)
