7a8a4152f4e4930d23be08cf0354c6f6b1e8d7ba
whysoq520 <53708390+whysoq520@users.noreply.github.com>
2019-10-17 23:56:50 -0400

    Update main.py

--- python-challenge/PyBank/main.py
+++ python-challenge/PyBank/main.py
@@ -9,89 +9,60 @@ csvpath= os.path.join("Resources", 'budget_data.csv')
 
 # Placeholders for re-formatted contents
 monthlist= [] 
+total_vote_number=0
 total_value = 0
+total_num_month = 0
 profit_losses_list = []
 monthly_change =[]
-grates_increase_in_profits = max (monthly_change)
-gratest_decrease_in_profits = min(monthly_change)
-
-
-
-
+# grates_increase_in_profits = max (monthly_change)
+# gratest_decrease_in_profits = min(monthly_change)
 
 #read file for data
-with open(csvpath, newline='') as csvfile:
-  csvreader = csv.reader(csvfile, delimiter=',')
+with open(csvpath, newline='') as bank:
+  reader = csv.reader(bank, delimiter=',')
 
-  #there is header, read the head row first
-  csv_header = next(csvreader)
-  #print(f"Header:{csv_header}")
-  #print(csvreader)
-  
+    #there is header, read the head row first
+  header = next(bank)
+  print(f"Header:{header}") 
 
-# print contents
-  for row in csvreader:
-    # Grab date and store it into a list
-    total_value = total_value + (row[1])
-    #monthlist.append(row[0])
-    monthlist = monthlist + [row[0]]
-    #print(monthlist)
-# The total number of months included in the dataset
-    total_num_month = len(monthlist)
-    print(total_num_month)
-    #why it print 1-86/ why print all lists? 
-
-# The net total amount of "Profit/Losses" over the entire period
+  for row in reader:
+    total_num_month = total_num_month+1
     total_value = total_value + int(row[1])
     #print(total_value)
+    #print(total_num_month)
     profit_losses_list.append(row[1])
-    #print(profit_losses_list)
+    for i in range((len(profit_losses_list))-1):
+      change = int(profit_losses_list[i+1]) - int(profit_losses_list[i])
+      monthly_change.append(change)
+      average_change = statistics.mean(monthly_change)
+      #print(average_change)
+      greatest_increase = max (monthly_change)
+      increase_index =monthly_change.index(greatest_increase)
+      print(increase_index)
+      greatest_increase_date = monthlist[increase_index]
+
+      greatest_decrease = min(monthly_change)
+      decrease_index =monthly_change.index(greatest_decrease)
+      greatest_decrease_date = monthlist[decrease_index]
+      print(greatest_decrease_date)
+
     
-
-#The average of the changes in "Profit/Losses" over the entire period
-for i in range((len(profit_losses_list))-1):
-  change = profit_losses_list[i+1] - profit_losses_list[i]
-  monthly_change.append(change)
-  average_change = statistics.mean(monthly_change)
-
-
-#for i in range(len(profit_losses)):
-# #The greatest increase in profits (date and amount) over the entire period
-  #greatest_increase = max(profit_losses)
-  greatest_increase = max (monthly_change)
-  increase_index =monthly_change.index(greatest_increase)
-  greatest_increase_date = monthlist[(increase_index+1)]
-  
-
-#The greatest decrease in losses (date and amount) over the entire period
-  greatest_decrease = min(monthly_change)
-  decrease_index =monthly_change.index(greatest_decrease)
-  greatest_decrease_date = monthlist[(decrease_index +1)]
-
 #path to store file
-  file_to_output = os.path.join("Financial Analysis.txt")
+file_to_output = os.path.join("Financial Analysis.txt")
 
-# Generate budget data Analysis Output
+# # # Generate budget data Analysis Output
 output = (
-  f"Financial Analysis\n"
-  f"-----------------\n"
-  f"Total Months: {total_num_month}\n"
-  f"Profit_losses Total: ${total_value}\n"
-  f"Average Change : ${average_change}\n"
-  f"Greatest Increase in Profits:{greatest_increase_date} (${greatest_increase})\n"
-  f"Greatest Decrease in Profits:{greatest_decrease_date} (${greatest_decrease})\n")
+f"Financial Analysis\n"
+f"----------------------------------------------\n"
+f"Total Months: {total_num_month}\n"
+f"Profit_losses Total: ${total_value}\n"
+f"Average Change : ${average_change}\n"
+f"Greatest Increase in Profits:{greatest_increase_date} (${greatest_increase})\n"
+f"Greatest Decrease in Profits:{greatest_decrease_date} (${greatest_decrease})\n")
 
-# Print all of the results (to terminal)
+# # # Print all of the results (to terminal)
 print(output)
 
-# Save the results to analysis text file
-with open(file_to_output, "a") as txt_file:
-    txt_file.write(output)
-
-  #Financial Analysis
-
-  #Total Months: 86
-  #Total: $38382578
-  #Average  Change: $-2315.12
-  #Greatest Increase in Profits: Feb-2012 ($1926159)
-  #Greatest Decrease in Profits: Sep-2013 ($-2196167)
+# # # Save the results to analysis text file
+with open(bankreport, "w") as txt_file:
+  txt_file.write(output)
