f2c7eaeb1ebc2b7128ab13104028677371785147
whysoq520 <53708390+whysoq520@users.noreply.github.com>
2019-11-03 12:16:58 -0500

    Update api_keys.py

--- python-api-challenge/WeatherPy/starter_code/api_keys.py
+++ python-api-challenge/WeatherPy/starter_code/api_keys.py
@@ -1,6 +1,10 @@
 # OpenWeatherMap API Key
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 api_key="3bbce4f306a61ab5f0f56e371bdce5b2"
+=======
+api_key = "179fccd0dec3d3cc43176c546faaa16b"
+>>>>>>> Stashed changes
 =======
 api_key = "179fccd0dec3d3cc43176c546faaa16b"
 >>>>>>> Stashed changes
